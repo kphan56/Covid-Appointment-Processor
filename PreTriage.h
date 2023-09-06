e7adac9215ca0d99107b9d1b03f732a5a6a09eb2
Kevin Phan <kphan6@hotmail.com>
2023-09-06 12:41:18 -0400

    Minor changes

--- PreTriage.h
+++ PreTriage.h
@@ -1,17 +1,8 @@
-/* Citation and Sources...
-Final Project Milestone 5
+/*-----------------------------------------------------------
 Module: PreTriage
 Filename: PreTriage.h
 Version 1.0
 Author	Kevin Phan
-Revision History
------------------------------------------------------------
-Date      Reason
-2020/4/10  Preliminary release
-2020/4/10  Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
 
 #ifndef SDDS_PRETRIAGE_H
